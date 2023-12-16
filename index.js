46a48291fa2810ca9f598832fffe17f81c9de9a4
sakiynam <mamatisayevasarvinoz@gmail.com>
2023-12-16 21:56:20 +0500

    Tasks

---
+++ index.js
@@ -0,0 +1,50 @@
+// // 1-task
+// let a = 8;
+// let b = 10;
+// let augment = 1;
+
+// while(a <= b){
+//     augment *= a;
+//     a++;
+// }
+
+// console.log(augment);
+
+
+
+// // 2-task
+// let i = 5898151;
+// let xonasoni = 0;
+
+// while(i > 1){
+//     i = i / 10;
+//     xonasoni += 1;
+// }
+
+// console.log(xonasoni);
+
+
+
+// // 3-task
+// let n = 10;
+// let multiply = 1;
+
+// let i = 1;
+// while(i <= n){
+//     multiply *= i;
+//     i++;
+// }
+// console.log(multiply);
+
+// // 4-task
+// let parol = prompt("Parolni kiriting");
+// let togriparol = 15874;
+
+// while(parol != togriparol){
+//     parol = prompt("Parol notogri, qayta urunib koring");
+// }
+
+// while(parol == togriparol){
+//     parol = alert("Parol togri");
+// }
+
